c99811e637f053ddc86f8ee02207b0ae3950d0be
tech-hack-rich <ricardoathleta@icloud.com>
2021-02-26 19:35:51 +0900

    account削除機能・編集機能

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,5 +1,7 @@
 class AccountsController < ApplicationController
   before_action :authenticate_user!, except: :index
+  before_action :find_account, only: [:show, :edit, :update, :destroy]
+  before_action :user_match, only: [:show, :edit, :update, :destroy]
   
   def index
     if user_signed_in?
@@ -21,14 +23,36 @@ class AccountsController < ApplicationController
   end
 
   def show
-    @account = Account.find(params[:id])
-    unless current_user == @account.user
+  end
+
+  def edit
+  end
+
+  def update
+    if @account.update(account_params)
+      redirect_to account_path(@account)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @account.destroy
       redirect_to root_path
     end
   end
+
 end
 
 private
   def account_params
     params.require(:account).permit(:name, :info).merge(user_id: current_user.id)
+  end
+
+  def find_account
+    @account = Account.find(params[:id])
+  end
+
+  def user_match
+    redirect_to root_path unless current_user == @account.user
   end
